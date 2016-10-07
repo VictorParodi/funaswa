1557b7e59ab527728a0b269980442426ce524ee8
VictorParodi <victor.parodi.montero@gmail.com>
2016-10-07 17:34:22 -0500

    rama master commit 5: nuevo diseño al 90%

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  alert('Bienvenido!, pero debes saber que aún estamos en construcción');
+
+  $('.special.cards .image').dimmer({
+    on: 'hover'
+  });
+
+  $('.ui.dropdown').dropdown();
+});

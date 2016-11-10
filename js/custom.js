01ebe06813d1e859fb6c95178de30f289c6954da
VictorParodi <victor.parodi.montero@gmail.com>
2016-11-10 03:39:33 -0500

    rama2 commit 1: nuevo diseño al 95%. Próximo a convinarse con la rama1

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  alert('Bienvenido!, pero debes saber que aún estamos en construcción');
+  //alert('Bienvenido!, pero debes saber que aún estamos en construcción');
 
   $('.special.cards .image').dimmer({
     on: 'hover'
